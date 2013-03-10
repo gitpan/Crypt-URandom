16c26daef4b9a70d1279403e84ce7bf66659f92a
David Dick <ddick@cpan.org>
2013-03-10 09:20:53 +0000

    Import of DDICK/Crypt-URandom-0.34 from CPAN.
    gitpan-cpan-distribution: Crypt-URandom
    gitpan-cpan-version:      0.34
    gitpan-cpan-path:         DDICK/Crypt-URandom-0.34.tar.gz
    gitpan-cpan-author:       DDICK
    gitpan-cpan-maturity:     released

--- lib/Crypt/URandom.pm
+++ lib/Crypt/URandom.pm
@@ -14,7 +14,7 @@ our @EXPORT_OK = qw(
 );
 our %EXPORT_TAGS = ( 'all' => \@EXPORT_OK, );
 
-our $VERSION = '0.33';
+our $VERSION = '0.34';
 our @CARP_NOT = ('Crypt::URandom');
 
 sub CRYPT_SILENT      { return 64; }               # hex 40
@@ -183,7 +183,7 @@ Crypt::URandom - Provide non blocking randomness
 
 =head1 VERSION
 
-This document describes Crypt::URandom version 0.33
+This document describes Crypt::URandom version 0.34
 
 
 =head1 SYNOPSIS
